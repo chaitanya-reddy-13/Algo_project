2811b607c947ad58e8affaa1fe7559169e4db5e6
chaitanya-reddy-13 <cs22b1052@iiitdm.ac.in>
2025-07-20 13:37:06 +0530

    frontend1

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/register/", { username, email, password });
+      await axios.post("/register/", { username, email, password }, { headers: { 'Content-Type': 'application/json' } });
       alert("Registration successful! Please login.");
       navigate("/login");
     } catch (err) {
